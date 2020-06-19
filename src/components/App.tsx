3b381f7d406bf0edfebfd852ec4bc485e2a06b23
nomad302 <sagar.nargundi@gmail.com>
2020-06-19 18:07:56 +0530

    PR comments resolved

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export interface HelloProps { greet: string; company: string }
+export interface HelloProps { greet: string; company: string };
 
 export const App = (props: HelloProps) =>
     <h1>{props.greet} To {props.company}</h1>;
