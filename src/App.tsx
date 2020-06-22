55bb9b662cad63088b8f11529cf9466c4ddef15f
nomad302 <sagar.nargundi@gmail.com>
2020-06-22 11:58:53 +0530

    remove state type from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import IHelloProps from 'interfaces/IAppProps'
 
-export default class App extends React.Component<IHelloProps, {}>{
+export default class App extends React.Component<IHelloProps>{
     public render(): JSX.Element {
         return (
             <h1>{this.props.greet} To {this.props.company}</h1>
